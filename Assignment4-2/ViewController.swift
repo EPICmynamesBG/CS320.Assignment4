c337759982831c3f7fe20acaf2ccd3e605508cfa
EPICmynamesBG <mynamesbg@gmail.com>
2015-11-10 20:37:51 -0500

    Initial Commit

---
+++ Assignment4-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment4-2
+//
+//  Created by Brandon Groff on 11/10/15.
+//  Copyright © 2015 Brandon Groff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
